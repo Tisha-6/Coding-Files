4e398689bc1218f9596988b889299045fa04372a
Tisha-6 <tishapranami6@gmail.com>
2025-12-03 14:38:02 +0530

    update

---
+++ Execl/execl_con.php
@@ -0,0 +1,12 @@
+
+
+<?php
+$input  = "file.xlsx";      // Your Excel file
+$output = "output.pdf";     // PDF you want to create
+
+$command = 'soffice --headless --convert-to pdf "'.$input.'" --outdir .';
+
+exec($command);
+
+echo "Converted to PDF successfully!";
+?>
