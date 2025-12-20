caa1a5bf40799749312419b81b46ec64a4b4a580
Tisha-6 <tishapranami6@gmail.com>
2025-12-20 14:15:46 +0530

    update

--- staff-attendances/admin/view_attendance.php
+++ staff-attendances/admin/view_attendance.php
@@ -17,40 +17,48 @@ if (!$result) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>View Attendance</title>
 </head>
+
 <body>
 
-<h2>Attendance Records</h2>
+    <h2>Attendance Records</h2>
 
-<table border="1">
-    <tr>
-        <th>Name</th>
-        <th>Department</th>
-        <th>Date</th>
-        <th>Status</th>
-    </tr>
-
-    <?php
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-    ?>
+    <table border="1">
         <tr>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['department']; ?></td>
-            <td><?php echo $row['date']; ?></td>
-            <td><?php echo $row['status']; ?></td>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Date</th>
+            <th>Status</th>
         </tr>
-    <?php
-        }
-    } else {
-        echo "<tr><td colspan='4'>No attendance records found</td></tr>";
-    }
-    ?>
-</table>
 
-<a href="dashboard.php">Back</a>
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+                <tr>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['department']; ?></td>
+                    <td><?php echo $row['date']; ?></td>
+                    <td><?php echo $row['status']; ?></td>
+
+                    <td>
+                        <span class="badge present">Present</span>
+                    </td>
+
+                </tr>
+        <?php
+            }
+        } else {
+            echo "<tr><td colspan='4'>No attendance records found</td></tr>";
+        }
+        ?>
+    </table>
+
+    <a href="dashboard.php">Back</a>
 
 </body>
+
 </html>
