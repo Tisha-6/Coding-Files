393755bbf870772ba84ad0ceea318f43186b9051
Tisha-6 <tishapranami6@gmail.com>
2025-12-23 15:20:22 +0530

    update

--- Support_ticket_system/config/db.php
+++ Support_ticket_system/config/db.php
@@ -1,8 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "support_system");
-
-if (!$conn) {
-    die("Database connection failed");
-}
 session_start();
+$conn = mysqli_connect("localhost", "root", "", "support_system");
 ?>
+
