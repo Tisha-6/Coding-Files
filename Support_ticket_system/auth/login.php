319c6839a0bbacfa59def31d9b362e9d4493f9fe
Tisha-6 <tishapranami6@gmail.com>
2025-12-26 13:59:13 +0530

    update

--- Support_ticket_system/auth/login.php
+++ Support_ticket_system/auth/login.php
@@ -1,28 +1,29 @@
 <?php
 include("../config/db.php");
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$email = trim($_POST['email']);
+$password = trim($_POST['password']);
 
 $query = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($conn, $query);
 
-if (!$result) {
-    die("Query Failed: " . mysqli_error($conn));
-}
-
 $user = mysqli_fetch_assoc($result);
 
-if (!$user) {
-    die("Email not found in database");
-}
+if ($user && $password == $user['password']) {
 
-if ($password == $user['password']) {
     $_SESSION['user_id'] = $user['id'];
     $_SESSION['role'] = $user['role'];
 
-    echo "Login successful";
+    // 🚀 REDIRECT BASED ON ROLE
+    if ($user['role'] == 'admin') {
+        header("Location: ../admin/dashboard.php");
+        exit();
+    } else {
+        header("Location: ../user/my_tickets.php");
+        exit();
+    }
+
 } else {
-    die("Password is incorrect");
+    echo "Invalid email or password";
 }
 ?>
